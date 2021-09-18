c5b3886f86515592936e24626627b721e727569b
yaambs <yanshbhardwaj14@gmail.com>
2021-09-18 15:45:50 +0530

    milestone2 achieved for python assignment1

--- assignment1.py
+++ assignment1.py
@@ -10,10 +10,19 @@ movies_ids = []
 
 
 for i in range(5):
-    movie_id = soup.select(".titleColumn")[i]('a')[0]['href'][7:-2]
+    movie_id = soup.select(".titleColumn")[i]('a')[0]['href'][7:-1]
     movies_ids.append(movie_id)
 
+# Fetching the synposis for the above movies and storings
+
+movies_synopsis = []
 
 
+for i in range(5):
+    request_movie_link = requests.get(f"https://www.imdb.com/title/{movies_ids[i]}/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=e31d89dd-322d-4646-8962-327b42fe94b1&pf_rd_r=SCCQEDQ3A70KXDN2HEMG&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=top&ref_=chttp_tt_{i+1}")
+    
+    format_response = bs4.BeautifulSoup(request_movie_link.text, 'lxml')
+    
+    movies_synopsis.append(format_response.select('.ipc-html-content')[1].getText())
 
-
+print(movies_synopsis)
